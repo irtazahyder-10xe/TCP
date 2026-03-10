b2618cc3088fd2a6eebb230ed35fc34bf6ec2072
irtaza-10x <irtaza.hyder@10xengineers.ai>
2026-03-10 11:37:47 +0500

    Update raw pointer computation to MMIO functions

--- src/aplic.c
+++ src/aplic.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "uart.h"
+#include "mmio.h"
+
 #define NULL 0
 
 /* Limit of IRQ_DOMAINS is the same as IRQ_DOMAINS_PER_SOCKET in hw/riscv/virt.h */
@@ -27,14 +28,14 @@ uintptr_t irq_domain_table[IRQ_DOMAINS] = {
 
 static uintptr_t src_irq_domain(uint32_t irq_src)
 {
-    uint32_t *sourcecfg;
+    uint32_t srccfg;
     bool D;
     uint8_t domain_idx = 0;
     do {
-        sourcecfg = (uint32_t *) (irq_domain_table[domain_idx] +
-                                  APLIC_SOURCECFG_BASE);
-        sourcecfg += irq_src - 1;
-        D = !!(*sourcecfg & APLIC_SOURCECFG_D);
+        srccfg = mmio_read_word(irq_domain_table[domain_idx] +
+                                APLIC_SOURCECFG_BASE,
+                                irq_src - 1);
+        D = !!(srccfg & APLIC_SOURCECFG_D);
     } while (D && (++domain_idx < IRQ_DOMAINS));
 
     return (domain_idx < IRQ_DOMAINS) ? irq_domain_table[domain_idx] : NULL;
@@ -44,37 +45,33 @@ void aplic_init(uint32_t irq_src_count)
 {
     /* ============ ROOT DOMAIN SETTINGS ============ */
     /* Setting mmsiaddrcfg & mmsiaddrcfgh */
-    uint32_t *mmsiaddrcfg = (uint32_t *) (irq_domain_table[0] + APLIC_MMSICFGADDR);
-    uint32_t *mmsiaddrcfgh = (uint32_t *) (irq_domain_table[0]  + APLIC_MMSICFGADDRH);
-    *mmsiaddrcfg = MACHINE_IF_ADDR >> 12;
-    *mmsiaddrcfgh = 0UL;
+    mmio_write_word(irq_domain_table[0] + APLIC_MMSICFGADDR, 0,
+                    MACHINE_IF_ADDR >> 12);
+    mmio_write_word(irq_domain_table[0] + APLIC_MMSICFGADDRH, 0, 0);
 
     /* Setting smsiaddrcfg & smsiaddrcfgh */
-    uint32_t *smsiaddrcfg = (uint32_t *) (irq_domain_table[0] + APLIC_SMSICFGADDR);
-    uint32_t *smsiaddrcfgh = (uint32_t *) (irq_domain_table[0] + APLIC_SMSICFGADDRH);
-    *smsiaddrcfg = SUPERVISOR_IF_ADDR >> 12;
-    *smsiaddrcfgh = 0UL;
+    mmio_write_word(irq_domain_table[0] + APLIC_SMSICFGADDR, 0,
+                    SUPERVISOR_IF_ADDR >> 12);
+    mmio_write_word(irq_domain_table[0] + APLIC_SMSICFGADDRH, 0, 0);
 
     /* Configuring sourcecfg */
     /* By default all sourcecfg show interrupt is at root & DETACH */
-    uint32_t *sourcecfg = (uint32_t *) (irq_domain_table[0] + APLIC_SOURCECFG_BASE);
-    for (int i = 0; i < irq_src_count; i++)
+    for (uint32_t i = 0; i < irq_src_count; i++)
     {
-        *(sourcecfg + i) |= APLIC_SOURCECFG_SM_DETACH;
+        mmio_write_word(irq_domain_table[0] + APLIC_SOURCECFG_BASE,
+                        i, APLIC_SOURCECFG_SM_DETACH);
     }
 
     /* Enabling all specified interrupt sources for root domain ONLY */
-    /* TODO: Align the irq_src_count */
     for (uint8_t i = 0; i < (irq_src_count >> 5); i++) {
         aplic_setie(irq_domain_table[0], i, 0xFFFFFFFF);
     }
 
     /* ============ COMMON DOMAIN SETTINGS ============ */
     /* Configuring domaincfg */
-    uint32_t *domaincfg;
     for (int i = 0; i < IRQ_DOMAINS; i++) {
-        domaincfg = (uint32_t *) (irq_domain_table[i] + APLIC_DOMAINCFG);
-        *domaincfg |= APLIC_DOMAINCFG_IE;
+        mmio_write_word(irq_domain_table[i] + APLIC_DOMAINCFG, 0,
+                        APLIC_DOMAINCFG_IE);
     }
 }
 
@@ -85,25 +82,21 @@ void aplic_send_msi(uint32_t irq_src, uint32_t hart_index,
         return;
     }
 
-    uintptr_t irq_domain_addr = src_irq_domain(irq_src);
-    if (irq_domain_addr == NULL) {
+    uintptr_t irq_domain = src_irq_domain(irq_src);
+    if (irq_domain == NULL) {
         return;
     }
-
-    uint32_t *target = (uint32_t *) (irq_domain_addr + APLIC_TARGET_BASE);
-    uint32_t *setipnum = (uint32_t *) (irq_domain_addr + APLIC_SETIPNUM);
 
     hart_index &= 0x3FFFUL;
     guest_index &= 0x3FUL;
     eiid &= 0x7FFUL;
 
+    uint32_t target = hart_index << APLIC_TARGET_HART_IDX_SHIFT;
+    target |= guest_index << APLIC_TARGET_GUEST_IDX_SHIFT;
+    target |= eiid;
 
-    target += irq_src - 1;
-    *target = hart_index << APLIC_TARGET_HART_IDX_SHIFT;
-    *target |= guest_index << APLIC_TARGET_GUEST_IDX_SHIFT;
-    *target |= eiid;
-
-    *setipnum = (uint32_t) irq_src;
+    mmio_write_word(irq_domain + APLIC_TARGET_BASE, irq_src - 1, target);
+    mmio_write_word(irq_domain + APLIC_SETIPNUM, 0, irq_src);
 }
 
 void aplic_send_Nmsi(uint32_t base_irq_src, uint32_t irq_count,
@@ -113,40 +106,34 @@ void aplic_send_Nmsi(uint32_t base_irq_src, uint32_t irq_count,
         return;
     }
 
-    uintptr_t irq_domain_addr = src_irq_domain(base_irq_src);
-    if (irq_domain_addr == NULL) {
+    uintptr_t irq_domain = src_irq_domain(base_irq_src);
+    if (irq_domain == NULL) {
         return;
     }
 
-    uint32_t *target = (uint32_t *) (irq_domain_addr + APLIC_TARGET_BASE);
-    uint32_t *setip = (uint32_t *) (irq_domain_addr + APLIC_SETIP_BASE);
-    uint32_t max_irq_src = base_irq_src + irq_count;
-    uint32_t index;
-    uint32_t bit_mask;
-
     hart_index &= 0x3FFFUL;
     guest_index &= 0x3FUL;
+    uint32_t target, setip_k, lsb_irq, bit_mask;
+    uint32_t max_irq_src = base_irq_src + irq_count;
 
-    target += base_irq_src - 1;
     for (uint32_t i = base_irq_src; (i < max_irq_src) && (i < IRQ_SRC_MAX); i++) {
-            *target = hart_index << APLIC_TARGET_HART_IDX_SHIFT;
-            *target |= guest_index << APLIC_TARGET_GUEST_IDX_SHIFT;
-            // Setting EID to value of the interrupt generated
-            *target |= (uint32_t) (i);
-            target++;
+        target = hart_index << APLIC_TARGET_HART_IDX_SHIFT;
+        target |= guest_index << APLIC_TARGET_GUEST_IDX_SHIFT;
+        /* Setting EID to value of the interrupt generated */
+        target |= (uint32_t) (i);
+        mmio_write_word(irq_domain + APLIC_TARGET_BASE, i - 1, target);
     }
 
-    setip += (base_irq_src >> 5);
     do {
-        index = base_irq_src % 32;
-        bit_mask = (uint32_t) (-1L << index);
-        if (index + irq_count < 32) {
-            bit_mask &= (uint32_t) ((1UL << (index + irq_count)) - 1);
+        setip_k = base_irq_src >> 5;
+        lsb_irq = base_irq_src % 32;
+        bit_mask = (uint32_t) (-1L << lsb_irq);
+        if (lsb_irq + irq_count < 32) {
+            bit_mask &= (uint32_t) ((1UL << (lsb_irq + irq_count)) - 1);
         }
-        *setip = bit_mask;
 
-        base_irq_src += 32 - index;
-        setip++;
+        mmio_write_word(irq_domain + APLIC_SETIP_BASE, setip_k, bit_mask);
+        base_irq_src += 32 - lsb_irq;
     } while (base_irq_src < max_irq_src);
 }
 
@@ -157,21 +144,21 @@ void aplic_conf_sourcecfg(uint32_t irq_src, uintptr_t irq_domain,
         return;
     }
 
-    /* As sourcecfg MMRs have 1st interrupt at address 0, and irq_src > 0 */
-    irq_src--;
-
-    uint32_t *sourcecfg = (uint32_t *) (irq_domain + APLIC_SOURCECFG_BASE);
-    sourcecfg += irq_src;
-    *sourcecfg = 0;
+    uint32_t sourcecfg = 0;
 
     /* As we are using MSI, SM either detached or inactive */
     if (D) {
-        *sourcecfg |= APLIC_SOURCECFG_D;
-        *sourcecfg |= child_index_or_sm & APLIC_SOURCECFG_CHILDIDX_MASK;
+        sourcecfg |= APLIC_SOURCECFG_D;
+        sourcecfg |= child_index_or_sm & APLIC_SOURCECFG_CHILDIDX_MASK;
     } else {
-        *sourcecfg |= child_index_or_sm & APLIC_SOURCECFG_SM_MASK;
-        /* Updating setie for new source irq_domain */
-         aplic_setie(irq_domain, irq_src >> 5, 0xFFFFFFFF);
+        sourcecfg |= child_index_or_sm & APLIC_SOURCECFG_SM_MASK;
+    }
+
+    mmio_write_word(irq_domain + APLIC_SOURCECFG_BASE, irq_src - 1, sourcecfg);
+
+    /* Updating setie for new source irq_domain */
+    if (!D) {
+        aplic_setie(irq_domain, irq_src >> 5, 0xFFFFFFFF);
     }
 }
 
@@ -188,24 +175,24 @@ void aplic_Nirq_delegate(uint32_t base_irq_src, uint32_t irq_count,
      * sourcecfg MMRs have 1st interrupt at address 0, and irq_src > 0 hence
      * requied to base_irq_src - 1 to find required sourcecfg MMR address
      */
-    uint32_t max_irq_src = base_irq_src + irq_count - 1;
-    uint32_t *parent_srccfg_addr = (uint32_t *) (src_irq_domain(base_irq_src)
-                                                 + APLIC_SOURCECFG_BASE);
+    uint32_t max_irq_src = base_irq_src + irq_count;
+    uintptr_t parent_srccfg_addr = src_irq_domain(base_irq_src) +
+                                   APLIC_SOURCECFG_BASE;
     if (!parent_srccfg_addr) {
         return;
     }
 
-    parent_srccfg_addr += base_irq_src - 1;
+    uint32_t child_srccfg, parent_srccfg;
 
-    uint32_t *child_srccfg_addr = (uint32_t *) (irq_domain + APLIC_SOURCECFG_BASE);
-    child_srccfg_addr += base_irq_src - 1;
+    for (uint32_t i = base_irq_src; i < max_irq_src; i++) {
+        parent_srccfg = APLIC_SOURCECFG_D;
+        parent_srccfg |= child_index & APLIC_SOURCECFG_CHILDIDX_MASK;
 
-    for (int i = 0; i < irq_count; i++) {
-        *(parent_srccfg_addr + i) = 0;
-        *(parent_srccfg_addr + i) |= APLIC_SOURCECFG_D;
-        *(parent_srccfg_addr + i) |= child_index & APLIC_SOURCECFG_CHILDIDX_MASK;
+        child_srccfg = APLIC_SOURCECFG_SM_DETACH;
 
-        *(child_srccfg_addr + i) = APLIC_SOURCECFG_SM_DETACH;
+        mmio_write_word(parent_srccfg_addr, i - 1, parent_srccfg);
+        mmio_write_word(irq_domain + APLIC_SOURCECFG_BASE, i - 1,
+                        child_srccfg);
     }
 
     /**
@@ -220,14 +207,10 @@ void aplic_Nirq_delegate(uint32_t base_irq_src, uint32_t irq_count,
 
 void aplic_setie(uintptr_t irq_domain, uint32_t k, uint32_t bit_mask)
 {
-    uint32_t *setie = (uint32_t *) (irq_domain + APLIC_SETIE_BASE);
-    setie += k;
-    *setie = bit_mask;
+    mmio_write_word(irq_domain + APLIC_SETIE_BASE, k, bit_mask);
 }
 
 void aplic_in_clrie(uintptr_t irq_domain, uint32_t k, uint32_t bit_mask)
 {
-    uint32_t *clrie = (uint32_t *) (irq_domain + APLIC_CLRIE_BASE);
-    clrie += k;
-    *clrie = bit_mask;
+    mmio_write_word(irq_domain + APLIC_CLRIE_BASE, k, bit_mask);
 }
